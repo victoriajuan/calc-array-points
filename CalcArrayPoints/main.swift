10670c09d32d2652f5a321a28cb309bed4d57901
Yiran Juan <vicky37@uw.edu>
2017-04-06 13:54:06 -0700

    initial commit

---
+++ CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by Yiran Juan on 4/6/17.
+//  Copyright © 2017 Yiran Juan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
